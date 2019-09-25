d6d44edd3528e52688de8ff39624ef4c99fd27ed
balajikadambi <balaji.kadambi@gmail.com>
2019-09-25 19:26:54 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+'''
+Created on 04-Sep-2019
+
+@author: bkadambi
+'''
+
+# -*- coding: UTF-8 -*-
+"""
+hello_flask: First Python-Flask webapp
+"""
+from flask import Flask  # From module flask import class Flask
+app = Flask(__name__)    # Construct an instance of Flask class for our webapp
+
+@app.route('/')   # URL '/' to be handled by main() route handler
+def main():
+    """Say hello"""
+    return 'Hello, world!'
+
+if __name__ == '__main__':  # Script executed directly?
+    print("Hello, World. Uses S2I to build the application.")
+    app.run(host="0.0.0.0", port=8080, debug=True,use_reloader=True)  # Launch built-in web server and run this Flask webapp
